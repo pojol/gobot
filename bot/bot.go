5a043143b7a8daa445a6828aaa684a8f88e84177
pojol <pojol@outlook.com>
2021-08-26 23:54:12 +0800

    # fix - 对 selector 节点的单步进行些特殊处理，如果执行某个节点成功，应该需要跳出当次的循环；

--- bot/bot.go
+++ bot/bot.go
@@ -327,6 +327,10 @@ func (b *Bot) RunStep() bool {
 	// step 中使用了sleep之后，会有多个goroutine执行接下来的程序
 	// fmt.Println(goid.Get())
 
+	if b.cur.Parent.Ty == "SelectorNode" && f {
+		b.cur.Parent.Step = len(b.cur.Parent.Children)
+	}
+
 	if f && b.cur.Step < len(b.cur.Children) {
 		// down
 		nextidx := b.cur.Step
