96eb96f9a60bd9f05179908b30e5f157919ccdf5
pojol <pojol@outlook.com>
2021-10-30 16:09:50 +0800

    fix : character set issues

--- editor/src/model/request.js
+++ editor/src/model/request.js
@@ -24,15 +24,28 @@ export function Post(url, methon, formData) {
   });
 }
 
+var str2utf8 = window.TextEncoder ? function(str) {
+  var encoder = new TextEncoder('utf8');
+  var bytes = encoder.encode(str);
+  var result = '';
+  for(var i = 0; i < bytes.length; ++i) {
+      result += String.fromCharCode(bytes[i]);
+  }
+  return result;
+} : function(str) {
+  return eval('\''+encodeURI(str).replace(/%/gm, '\\x')+'\'');
+}
+
 export function PostBlob(url, methon, name, data) {
   return new Promise(function (resolve, reject) {
+    var headers = new Headers();
+    headers.append("Content-Type", "text/html");
+    headers.append("FileName", str2utf8(name));
+
     fetch(url + methon, {
       method: "POST",
       mode: "cors",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        FileName: name,
-      },
+      headers: headers,
       body: data,
     })
       .then((response) => {
