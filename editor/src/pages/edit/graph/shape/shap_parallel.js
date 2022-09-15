2fb8592215119170b1bb71568ffb102b19c350ba
pojol <pojol@outlook.com>
2022-09-15 20:50:50 +0800

    tmp

---
+++ editor/src/pages/edit/graph/shape/shap_parallel.js
@@ -0,0 +1,53 @@
+import { Shape } from "@antv/x6";
+
+class ParallelNode extends Shape.Rect {}
+
+ParallelNode.config({
+  attrs: {
+    root: {
+      magnet: true,
+    },
+    body: {
+      fill: "#f5f5f5",
+      stroke: "#5AA897",
+      strokeWidth: 1,
+    },
+    label: {
+      text: "Parallel",
+    },
+    type : "ParallelNode"
+  },
+  width: 30,
+  height: 30,
+  ports: {
+    items: [
+      {
+        group: "out",
+      },
+    ],
+    groups: {
+      out: {
+        position: {
+          name: "bottom",
+        },
+        attrs: {
+          portBody: {
+            magnet: true,
+            r: 5,
+            fill: "#fff",
+            stroke: "#3199FF",
+            strokeWidth: 1,
+          },
+        },
+      },
+    },
+  },
+  portMarkup: [
+    {
+      tagName: "circle",
+      selector: "portBody",
+    },
+  ],
+});
+
+export default ParallelNode;
