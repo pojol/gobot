210193fca70abf176bc696ea1ebbaa1ea76f1c36
taotao <taotao@outlook.com>
2023-12-08 21:45:53 +0800

    feat : print ping

--- editor/src/task/heart.tsx
+++ editor/src/task/heart.tsx
@@ -13,10 +13,16 @@ import { setHeartColor } from "@/models/config";
 const heart = async (): Promise<boolean> => {
 
     let heartStatus = false
+    let ping: number | null = null;
+    const startTime = Date.now(); // 记录发送请求的时间戳
+
     try {
         const res = await axios.get(localStorage.remoteAddr + "/health", {});
         if (res.status === 200) {
             heartStatus = true
+            const endTime = Date.now(); // 记录收到响应的时间戳
+            ping = endTime - startTime; // 计算时间差，即ping值
+            console.info("health", ping, "ms")
         }
     } catch (err) {
         console.info("health", err)
